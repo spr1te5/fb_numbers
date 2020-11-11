c38653b41118dea5e3038a34a599324722cfe314
Eugene Porodnikov <questeug@gmail.com>
2020-11-11 16:25:05 +0300

    Data source processing refactored: STDIN and String content separated.

--- lib/numbers_finder.rb
+++ lib/numbers_finder.rb
@@ -1,21 +1,50 @@
 require 'numbers_finder/version'
 
 module NumbersFinder
+  extend self
+  
   class Error < StandardError; end
 
-  extend self
+  class StdInChunksSource
+    attr_accessor :chunk_size
 
-  # Извлекает из потока текста максимальные целые числа длиной не более заданного значения.
+    def initialize(size = 4096)
+      self.chunk_size = size
+    end
+
+    def each_char
+      begin
+        loop do
+          STDIN.read_nonblock(chunk_size).each_char { |char| yield(ch) }
+        end
+      rescue
+      end
+    end
+  end
+
+  class StringChunksSource
+    attr_accessor :string
+    
+    def initialize(string)
+      self.string = string
+    end
+
+    def each_char
+      string.each_char { |ch| yield(ch) }
+    end
+  end
+
+  # Извлекает из источника текста максимальные целые числа длиной не более заданного значения.
   # == Параметры:
-  # str:: поток с текстовыми данными.
   # max_numbers:: количество максимальных чисел.
   # max_number_len:: максимальная длина числа в символах.
+  # source:: поток с текстовыми данными (по умолчанию - STDIN).  
   # == Результат:
   # отсортированый по возрастанию массив, содержащий максимальные целые числа.
-  def extract_maximum_numbers_from_string str, max_numbers, max_number_len
+  def extract_maximum_numbers_from_stream(max_numbers, max_number_len, source = StdInChunksSource.new)
     result = []
 
-    extract_numbers_from_stream(str, max_numbers, max_number_len) { |number| 
+    scan_chunks_for_numbers(source, max_numbers, max_number_len) { |number| 
       result.sort! { |num1, num2| num1 <=> num2 }  
       if result.size < max_numbers
         result << number
@@ -30,30 +59,27 @@ module NumbersFinder
     result.map(&:to_i)
   end
 
-  # :nodoc:
-  def extract_numbers_from_stream str, max_numbers, max_number_len 
+  def scan_chunks_for_numbers(source, max_numbers, max_number_len)
     last_number = ''
-    str.each_char
-       .lazy
-       .each { |ch|
-          catch_number = false
+    source.each_char { |ch|
+      catch_number = false
 
-          if ch =~ /[0-9]/
-            last_number << ch
-            catch_number = true if last_number.length == max_number_len
-          elsif last_number.length > 0
-            catch_number = true
-          end
+      if ch =~ /[0-9]/
+        last_number << ch
+        catch_number = true if last_number.length == max_number_len
+      elsif last_number.length > 0
+        catch_number = true
+      end
 
-          if catch_number
-            yield "%0#{max_number_len}d" % last_number
-            last_number = ''
-          end
-       }
+      if catch_number
+        yield "%0#{max_number_len}d" % last_number
+        last_number = ''
+      end
+    }
 
-     #The last symbol is a digit  
-     if last_number.length > 0
-       yield "%0#{max_number_len}d" % last_number
-     end
+    #The last symbol is a digit  
+    if last_number.length > 0
+      yield "%0#{max_number_len}d" % last_number
+    end
   end
 end
--- spec/numbers_finder_spec.rb
+++ spec/numbers_finder_spec.rb
@@ -10,32 +10,32 @@ RSpec.describe NumbersFinder do
     let(:max_number_len) { 4 }
 
     it 'when number of digits less than required' do
-      numbers = subject.extract_maximum_numbers_from_string('text 666', max_numbers, max_number_len)
+      numbers = subject.extract_maximum_numbers_from_stream(max_numbers, max_number_len, NumbersFinder::StringChunksSource.new('text 666'))
       expect(numbers).to eq([666])
     end
 
     it 'from empty string' do
-      numbers = subject.extract_maximum_numbers_from_string('', max_numbers, max_number_len)
+      numbers = subject.extract_maximum_numbers_from_stream(max_numbers, max_number_len, NumbersFinder::StringChunksSource.new(''))
       expect(numbers).to eq([])
     end
 
     it 'from text not countaining digits' do
-      numbers = subject.extract_maximum_numbers_from_string('text text text', max_numbers, max_number_len)
+      numbers = subject.extract_maximum_numbers_from_stream(max_numbers, max_number_len, NumbersFinder::StringChunksSource.new('text text text'))
       expect(numbers).to eq([])
     end
 
     it 'when non-repeated numbers are glued' do
-      numbers = subject.extract_maximum_numbers_from_string('text 88889999777 text', max_numbers, max_number_len)
+      numbers = subject.extract_maximum_numbers_from_stream(max_numbers, max_number_len, NumbersFinder::StringChunksSource.new('text 88889999777 text'))
       expect(numbers).to eq([8888, 9999])
     end
 
     it 'when repeated numbers are glued' do
-      numbers = subject.extract_maximum_numbers_from_string('text 88889999777 text 777788889999 text', max_numbers, max_number_len)
+      numbers = subject.extract_maximum_numbers_from_stream(max_numbers, max_number_len, NumbersFinder::StringChunksSource.new('text 88889999777 text 777788889999 text'))
       expect(numbers).to eq([8888, 9999])
     end
 
     it 'when non-repeated numbers are not glued' do
-      numbers = subject.extract_maximum_numbers_from_string('text 1111 text 222 text 3333', max_numbers, max_number_len)
+      numbers = subject.extract_maximum_numbers_from_stream(max_numbers, max_number_len, NumbersFinder::StringChunksSource.new('text 1111 text 222 text 3333'))
       expect(numbers).to eq([1111, 3333])
     end
   end
